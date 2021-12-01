f79ff8695088e88f498e9bf54752efeb34cc3156
samrodd <loughlinrodd@gmail.com>
2021-12-01 15:10:47 -0500

    2nd Commit Updates modesl for students and campuses

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -8,9 +8,22 @@ const Campus = db.define("campus", {
     allowNull: false
   },
 
+  imgUrl: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    defaultValue: 'https://admission.brown.edu/sites/g/files/dprerj526/files/styles/ultrawide_med/public/2020-06/Hosted-Virtual-Campus-Tour-banner-larger.jpg?h=298f6650&itok=55wGTeb3'
+  },
+
+  address: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+
   description: {
-    type: Sequelize.STRING,
+    type: Sequelize.TEXT,
   }
+
+
 
 });
 
--- database/models/Student.js
+++ database/models/Student.js
@@ -11,6 +11,21 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  imgUrl: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    defaultValue: 'https://admission.brown.edu/sites/g/files/dprerj526/files/styles/ultrawide_med/public/2020-06/Hosted-Virtual-Campus-Tour-banner-larger.jpg?h=298f6650&itok=55wGTeb3'
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL
   }
 
 });
